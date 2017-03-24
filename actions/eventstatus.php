b6de56448c2ba6183b84a53c6913c193393dc814
Jorge Rodriguez <jito.rodriguez@outlook.com>
2017-03-24 17:29:21 -0400

    Adds functionality to update status of events for super admin. Requires constraint converage of only super admins.

---
+++ actions/eventstatus.php
@@ -0,0 +1,104 @@
+<?php
+
+	header("content-type:application/json");
+    include_once "config.php";
+
+	$data = json_decode(file_get_contents("php://input"));
+
+	$conn = new mysqli($host_name, $user_name, $password, $database);
+	// Check connection
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	} 
+
+	if($_SERVER['REQUEST_METHOD'] === 'GET')
+	{
+		//$id = ( isset( $_GET['user_id'] ) && is_numeric( $_GET['user_id'] ) ) ? intval( $_GET['user_id'] ) : -1;
+
+		if(isset($_GET['user_id']) && isset($_GET['false']))
+		{
+			// retrieve all events not approved
+			$user_id = $_GET['user_id'];
+
+			$sql = "SELECT * FROM eventmeeting WHERE approved_by_superadmin = 0";
+
+			$results = $conn->query($sql);
+
+			$rows = array();
+
+			if ($results->num_rows > 0) 
+			{
+				// output data of each row
+				while($r = $results->fetch_assoc()) {
+					$rows[] = $r;
+					//$rows[$r->username] = "{comment: '" . $r->comment . "', date: '" . $r->date . "'}";
+				}
+				print json_encode($rows);
+			} 
+			else 
+			{
+				echo $conn->error;
+				print json_encode("{}");
+			}
+		}
+		else if(isset($_GET['user_id']))
+		{
+			//Get both approved and non approved events
+			$user_id = $_GET['user_id'];
+
+			$sql = "SELECT * FROM eventmeeting";
+
+			$results = $conn->query($sql);
+
+			$rows = array();
+
+			if ($results->num_rows > 0) 
+			{
+				// output data of each row
+				while($r = $results->fetch_assoc()) {
+					$rows[] = $r;
+					//$rows[$r->username] = "{comment: '" . $r->comment . "', date: '" . $r->date . "'}";
+				}
+				print json_encode($rows);
+			} 
+			else 
+			{
+				echo $conn->error;
+				print json_encode("{}");
+			}
+		}
+	}
+	else if($_SERVER['REQUEST_METHOD'] === 'POST')
+	{
+
+		$cmdtype = $data->type;
+
+		//CREATE SCENARIO
+		if($cmdtype === "update")
+		{
+			$id = $data->user_id;
+			$event_id = $data->event_id;
+			$approval = $data->status;
+
+			$sql = "UPDATE eventmeeting SET approved_by_superadmin = " . $approval . " WHERE e_id = " . $event_id . ";";
+
+			$results = $conn->query($sql);
+
+			if($results)
+			{
+				echo "Success updated event status.";
+				$response_array['status'] = "success";
+				$response_array['message'] = "";
+				print json_encode($response_array);
+			}
+			else
+			{
+				echo "Unsuccessfully updated event status.";
+				$response_array['status'] = "failure updating event status";
+				$response_array['message'] = $conn->error;
+				print json_encode($response_array);
+			}
+		
+		}
+	}
+?>
