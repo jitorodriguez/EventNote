07a89ba84cd5ea1e9edee7d689aee7a8550371e4
Jorge Rodriguez <jito.rodriguez@outlook.com>
2017-03-25 20:36:48 -0400

    Updates user to encode login info in POST query.

--- actions/User.php
+++ actions/User.php
@@ -14,75 +14,77 @@
 	if($_SERVER['REQUEST_METHOD'] === 'GET'){
 		//The request is using a GET method
 
-		if(isset( $_GET['user']) && isset( $_GET['pass']))
-		{
-			$user = $_GET['user'];
-			$pass = $_GET['pass'];
+		$id = $_GET['user_id'];
 
-			$sql = "SELECT U.s_id FROM users U WHERE U.username = '" . $user . "' AND U.password = '" . $pass . "';"; 
+		$sql = "SELECT U.username, U.name, U.email, S.major, S.minor, S.description FROM users U, student S WHERE U.s_id = " . $id . " AND U.s_id = S.s_id";
 
-			$result = $conn->query($sql);
+		$result = $conn->query($sql);
 
-			$rows = array();
+		$rows = array();
 
-			if ($result->num_rows > 0) {
-			    // output data of each row
-			    while($r = $result->fetch_assoc()) {
-			        $rows[] = $r;
-			    }
-			    print json_encode($rows);
-			} else {
-			    print json_encode("{}");
-			}
-		}
-		else
-		{
-			$id = $_GET['user_id'];
-
-			$sql = "SELECT U.username, U.name, U.email, S.major, S.minor, S.description FROM users U, student S WHERE U.s_id = " . $id . " AND U.s_id = S.s_id";
-
-			$result = $conn->query($sql);
-
-			$rows = array();
-
-			if ($result->num_rows > 0) {
-			    // output data of each row
-			    while($r = $result->fetch_assoc()) {
-			        $rows[] = $r;
-			    }
-			    print json_encode($rows);
-			} else {
-			    print json_encode("{}");
-			}
+		if ($result->num_rows > 0) {
+		    // output data of each row
+		    while($r = $result->fetch_assoc()) {
+		        $rows[] = $r;
+		    }
+		    print json_encode($rows);
+		} else {
+		    print json_encode("{}");
 		}
 		
 	}
 	else if($_SERVER['REQUEST_METHOD'] === 'POST')
 	{
-	    $id = $data->user_id;
-	    $username = $data->username;
-	    $email = $data->email;
-	    $name = $data->name;
-	    $pass = $data->password;
-	    $major = $data->major;
-	    $minor = $data->minor;
-	    $description = $data->description;
+		$cmdtype = $data->type;
 
-	    $sql = "UPDATE users U, student S SET U.username='" . $username . "', U.email='" . $email . "', U.name='" . $name . "', U.password ='" . $pass . "', S.major = '" . $major . "', S.minor ='" . $minor . "', S.description = '" . $description . "' WHERE U.s_id =" . $id . " AND S.s_id = U.s_id;";
+		if($cmdtype === "update")
+		{
+		    $id = $data->user_id;
+		    $username = $data->username;
+		    $email = $data->email;
+		    $name = $data->name;
+		    $pass = $data->password;
+		    $major = $data->major;
+		    $minor = $data->minor;
+		    $description = $data->description;
 
-	    echo $sql;
+		    $sql = "UPDATE users U, student S SET U.username='" . $username . "', U.email='" . $email . "', U.name='" . $name . "', U.password ='" . $pass . "', S.major = '" . $major . "', S.minor ='" . $minor . "', S.description = '" . $description . "' WHERE U.s_id =" . $id . " AND S.s_id = U.s_id;";
 
-		if($conn->query($sql) === TRUE){
+		    echo $sql;
 
-			$response_array['status'] = "success";
-			$response_array['message'] = "";
-			print json_encode($response_array);
+			if($conn->query($sql) === TRUE){
+
+				$response_array['status'] = "success";
+				$response_array['message'] = "";
+				print json_encode($response_array);
+			}
+			else{
+
+				$response_array['status'] = "failure";
+				$response_array['message'] = $conn->error;
+				print json_encode($response_array);
+			}
 		}
-		else{
+		else if($cmdtype === "log")
+		{
+				$user = $data->user;
+				$pass = $data->pass;
 
-			$response_array['status'] = "failure";
-			$response_array['message'] = $conn->error;
-			print json_encode($response_array);
+				$sql = "SELECT U.s_id FROM users U WHERE U.username = '" . $user . "' AND U.password = '" . $pass . "';"; 
+
+				$result = $conn->query($sql);
+
+				$rows = array();
+
+				if ($result->num_rows > 0) {
+				    // output data of each row
+				    while($r = $result->fetch_assoc()) {
+				        $rows[] = $r;
+				    }
+				    print json_encode($rows);
+				} else {
+				    print json_encode("{}");
+				}
 		}
 	}
 	
